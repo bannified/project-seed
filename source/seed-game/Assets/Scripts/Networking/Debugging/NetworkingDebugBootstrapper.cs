f441093b6825860bfc8b53d77325aa060f45c6d3
bannified <freedomzelda_dc@hotmail.com>
2020-04-24 22:41:35 +0800

    Add NetworkingDebugBootstrapper

---
+++ source/seed-game/Assets/Scripts/Networking/Debugging/NetworkingDebugBootstrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Mirror;
+
+public class NetworkingDebugBootstrapper : MonoBehaviour
+{
+    enum ConnectionType
+    {
+        Host,
+        Client,
+        Server
+    }
+
+    [EnumToggleButtons]
+    [SerializeField]
+    private ConnectionType connectionType;
+
+    [Required][AssetsOnly]
+    [SerializeField]
+    private SeedGameNetworkManager NetworkManagerPrefab;
+
+    [ReadOnly]
+    [SerializeField]
+    private SeedGameNetworkManager networkManager;
+
+    [SerializeField]
+    private string networkAddress = "localhost";
+
+    private void Awake()
+    {
+         networkManager = Instantiate<SeedGameNetworkManager>(NetworkManagerPrefab);
+
+        switch (connectionType)
+        {
+            case ConnectionType.Host:
+                networkManager.StartHost();
+                break;
+            case ConnectionType.Client:
+                networkManager.networkAddress = networkAddress;
+                networkManager.StartClient();
+                break;
+            case ConnectionType.Server:
+                networkManager.StartServer();
+                break;
+            default:
+                break;
+        }
+
+    }
+}
